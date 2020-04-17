bd3fe5361234966886eac069b452a2797581cf34
aivan6842 <alexivanov15@icloud.com>
2020-04-17 14:53:16 -0400

    Added new Style for Pop Up Window

--- app/src/main/java/com/example/tictactoe/EndGamePopUp.java
+++ app/src/main/java/com/example/tictactoe/EndGamePopUp.java
@@ -1,6 +1,7 @@
 package com.example.tictactoe;
 
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +11,11 @@ public class EndGamePopUp extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_end_game_pop_up);
+
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+        int width = dm.widthPixels;
+        int height = dm.heightPixels;
+        getWindow().setLayout((int)(width*.8), (int)(height*0.5));
     }
 }
